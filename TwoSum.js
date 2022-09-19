4e5d2d5be0c487a787ff7832c86231411f2d5402
James L <59751464+Jazz1016@users.noreply.github.com>
2022-09-19 10:44:52 -0600

    TwoSum done with hash map

---
+++ TwoSum.js
@@ -0,0 +1,17 @@
+// Hash Map
+var twoSum = function (nums, target) {
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let val = nums[i];
+    hash[val] = i;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let potentialKey = target - nums[i];
+
+    if (hash[potentialKey] && hash[potentialKey] !== i) {
+      return [i, hash[potentialKey]];
+    }
+  }
+};
